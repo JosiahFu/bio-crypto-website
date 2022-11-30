43201e2748b5fac5a7b7b9af22bacd01aaca72f6
JosiahFu <josiahfu@gmail.com>
2022-11-30 15:09:45 -0800

    Fix table of contents and jittering

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 const headerTitle = document.getElementById('title');
+const titleTestLarge = document.getElementById('title-test-large');
+const titleTestSmall = document.getElementById('title-test-small');
+const sidebar = document.getElementById('sidebar');
+
+document.getElementById('header').style.backgroundPositionY = document.documentElement.scrollTop / 2 + 'px';
+
+for (let i of document.getElementsByTagName('h2')) {
+    const nav = document.createElement('div');
+    nav.appendChild(document.createTextNode(i.innerText));
+    nav.setAttribute('target', i.id);
+    nav.classList.add('nav');
+    nav.addEventListener('click', e => {
+        for (let j of document.getElementsByClassName('selected')) {
+            j.classList.remove('selected');
+        }
+        e.target.classList.add('selected');
+        const target = document.getElementById(e.target.getAttribute('target'));
+        window.scrollTo({
+            top: target.offsetTop - titleTestSmall.clientHeight - 20,
+            behavior: "smooth"
+        });
+        
+    });
+    sidebar.appendChild(nav);
+}
+
 window.onscroll = () => {
-    headerTitle.style.fontSize = '4em';
-    if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight - (headerTitle.clientHeight + 20)) {
+    if (document.documentElement.scrollTop > window.innerHeight - (titleTestLarge.clientHeight + 20)) {
         headerTitle.classList.add('stick');
         headerTitle.style.top = '';
     } else {
         headerTitle.classList.remove('stick');
         headerTitle.style.top = (window.innerHeight + document.documentElement.scrollTop) / 2 + 'px';
     }
-    headerTitle.style.fontSize = '';
+    if (document.documentElement.scrollTop + titleTestSmall.clientHeight > window.innerHeight) {
+        sidebar.classList.add('stick');
+        sidebar.style.top = titleTestSmall.clientHeight + 'px';
+    } else {
+        sidebar.classList.remove('stick');
+    }
     document.getElementById('header').style.backgroundPositionY = document.documentElement.scrollTop / 2 + 'px';
 };
