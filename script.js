65978e08b716ed158bbd9c4753fc5fd4db71f400
JosiahFu <josiahfu@gmail.com>
2022-12-03 23:57:16 -0800

    Styling/layout

--- script.js
+++ script.js
@@ -5,23 +5,33 @@ const sidebar = document.getElementById('sidebar');
 
 document.getElementById('header').style.backgroundPositionY = document.documentElement.scrollTop / 2 + 'px';
 
-for (let i of document.getElementsByTagName('h2')) {
+const navClick = e => {
+    for (let j of document.getElementsByClassName('selected')) {
+        j.classList.remove('selected');
+    }
+    e.target.classList.add('selected');
+    const target = document.getElementById(e.target.getAttribute('target'));
+    window.scrollTo({
+        top: target.offsetTop - titleTestSmall.clientHeight - 20,
+        behavior: "smooth"
+    });
+};
+
+['h2', 'h3'].forEach(e => {
+    for (let i of document.getElementsByTagName(e)) {
+        i.classList.add('heading');
+    }
+});
+
+for (let i of document.getElementsByClassName('heading')) {
     const nav = document.createElement('div');
     nav.appendChild(document.createTextNode(i.innerText));
     nav.setAttribute('target', i.id);
     nav.classList.add('nav');
-    nav.addEventListener('click', e => {
-        for (let j of document.getElementsByClassName('selected')) {
-            j.classList.remove('selected');
-        }
-        e.target.classList.add('selected');
-        const target = document.getElementById(e.target.getAttribute('target'));
-        window.scrollTo({
-            top: target.offsetTop - titleTestSmall.clientHeight - 20,
-            behavior: "smooth"
-        });
-        
-    });
+    if (i.tagName == 'H3') {
+        nav.classList.add('subheading');
+    }
+    nav.addEventListener('click', navClick);
     sidebar.appendChild(nav);
 }
 
